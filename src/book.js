503c8640480424a330aef25ace19c5cd790ec4e3
Nicole Lam <Nicolelam8891@gmail.com>
2023-07-08 13:00:39 -0600

    Add functions for books and deleted all skips

--- src/book.js
+++ src/book.js
@@ -1,12 +1,53 @@
-function createTitle() {
-  
+function createTitle(newTitle) {
+  newTitle = `The ${newTitle}`
+  return newTitle
 }
 
+function buildMainCharacter(characterName, characterAge, characterPronouns) {
+  var character = {
+    name: characterName,
+    age: characterAge,
+    pronouns: characterPronouns
+  }
+  return character; 
+}
+
+function saveReview(comment, reviews){
+  console.log("comment", comment);
+  console.log("reviews", reviews);
+  
+    if (reviews.includes(comment)) {
+      return reviews;
+    } else {
+      reviews.push(comment);
+      return reviews;
+    }
+  }
+
+function calculatePageCount(title) {
+ total = title.length * 20 
+  return total
+}
+  
+function writeBook(bookTitle, bookCharacter, genre) {
+    var book = {
+      title: bookTitle, 
+      mainCharacter: bookCharacter,
+      pageCount: calculatePageCount(bookTitle),
+      genre: genre
+    };
+    return book;
+}
+
+function editBook(ghoulBook) {
+  ghoulBook.pageCount = ghoulBook.pageCount * (3/4);
+    return ghoulBook;
+}
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
